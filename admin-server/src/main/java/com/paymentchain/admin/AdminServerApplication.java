376bb5af7c8e7055b8e7989883b331acf21c672a
JohanaLl <johisllanos@gmail.com>
2025-04-20 13:12:53 -0500

    Se agrega el microservicio de Admin server para saber la salud de cada uno de los ms

---
+++ admin-server/src/main/java/com/paymentchain/admin/AdminServerApplication.java
@@ -0,0 +1,24 @@
+package com.paymentchain.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+}
