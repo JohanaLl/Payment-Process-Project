5113d88451160a1655f524d1a91cc191816e75b6
JohanaLl <johisllanos@gmail.com>
2025-08-23 19:37:27 -0500

    Add Spring Security in Billing service

--- billing-service/src/main/java/com/paymentchain/billing/BillingServiceApplication.java
+++ billing-service/src/main/java/com/paymentchain/billing/BillingServiceApplication.java
@@ -2,13 +2,14 @@ package com.paymentchain.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BillingServiceApplication {
 
 	public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class BillingServiceApplication {
 	}
 
 	@Bean
-	 public OpenAPI customOpenAPI() {
+	public OpenAPI customOpenAPI() {
 	   return new OpenAPI()
 	          .components(new Components())
 	          .info(new Info()
